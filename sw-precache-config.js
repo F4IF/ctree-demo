dfb1000a18cd5523fe593a3d0b8805fc85821bbf
Chris Schmitz <cschmitz@innovativefuture.org>
2020-05-03 01:02:42 -0700

    -fix for app loading errors

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,7 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-loader.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.js'
   ],
   navigateFallback: '/index.html'
 };
